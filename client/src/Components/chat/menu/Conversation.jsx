7176d5f84210e1f5f4e98f13c1fad133384a7c74
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-28 13:13:32 +0530

    Conversation ui edit

--- client/src/Components/chat/menu/Conversation.jsx
+++ client/src/Components/chat/menu/Conversation.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
+const Component = styled(Box)`
+  display: flex;
+  height: 50px;
+  cursor: pointer;
+  margin: 10px 0;
+`;
+const ImageBox = styled(Box)`
+  padding: 0;
+`;
+const Image = styled("img")({
+  width: 80,
+  height: 50,
+  padding: "0px 14px",
+  borderRadius: "50%",
+  objectFit: "cover",
+});
 
 const Conversation = ({ user }) => {
   return (
     <>
-      <Box>
-        <Box>
-          <img src={user.picture} alt="dp" />
-        </Box>
+      <Component>
+        <ImageBox>
+          <Image src={user.picture} alt="dp" />
+        </ImageBox>
         <Box>
           <Typography>{user.name}</Typography>
         </Box>
-      </Box>
+      </Component>
     </>
   );
 };
--- client/src/Components/chat/menu/Conversations.jsx
+++ client/src/Components/chat/menu/Conversations.jsx
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import { Box, styled, Divider } from "@mui/material";
 import { getUsers } from "../../../service/Api";
 import Conversation from "./Conversation";
+import { AccountContext } from "../../../context/AccountProvider";
+
+const Component = styled(Box)`
+  overflow: overlay;
+  height: 85vh;
+`;
+const StyleDivider = styled(Divider)`
+  margin: 0 0 0 70px;
+  background: #e9edef;
+  opacity: 0.6;
+`;
 
 const Conversations = () => {
   const [users, setUsers] = useState([]);
+
+  const { account } = useContext(AccountContext);
+
   useEffect(() => {
     const fetchData = async () => {
       let response = await getUsers();
@@ -12,14 +26,20 @@ const Conversations = () => {
     };
     fetchData();
   }, []);
-  console.log(users);
+  // console.log(users);
   return (
     <>
-      <Box>
-        {users.map((user) => {
-          return <Conversation user={user} />;
-        })}
-      </Box>
+      <Component>
+        {users.map(
+          (user) =>
+            user.sub !== account.sub && (
+              <>
+                <Conversation user={user} />
+                <StyleDivider />
+              </>
+            )
+        )}
+      </Component>
     </>
   );
 };
