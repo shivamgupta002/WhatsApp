ed92b05d13f65a9416f0bd1be5d2c3cbe35b93cd
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-28 17:12:14 +0530

    Footer Ui complete

---
+++ client/src/Components/chat/chat/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Box, styled, InputBase } from "@mui/material";
+import { EmojiEmotionsOutlined, AttachFile, Mic } from "@mui/icons-material";
+const Container = styled(Box)`
+  height: 55px;
+  background: #ededed;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 0 15px;
+  & > * {
+    margin: 5px;
+    color: #919191;
+  }
+`;
+const SearchBox = styled(Box)`
+  background: #ffffff;
+  border-radius: 18px;
+  width: calc(94% - 100px);
+`;
+const InputField = styled(InputBase)`
+  width: 100%;
+  padding: 20px;
+  height: 20px;
+  padding-left: 25px;
+  font-size: 14px;
+`;
+const ClipIcon=styled(AttachFile)`
+transform:rotate(40deg);
+`;
+
+const Footer = () => {
+  return (
+    <>
+      <Container>
+        <EmojiEmotionsOutlined />
+        <ClipIcon />
+        <SearchBox>
+          <InputField placeholder="Type a Message" />
+        </SearchBox>
+        <Mic />
+      </Container>
+    </>
+  );
+};
+
+export default Footer;
