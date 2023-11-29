f3cebbae307425b6f445d243b5de977e53663062
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-29 15:43:22 +0530

    add route of message controller

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { addUser, getUser } from "../controller/user-controller.js";
-import { newConversation } from "../controller/conversation-controller.js";
+import { newMessage } from "../controller/message-controller.js";
+import {
+  newConversation,
+  getConversation,
+} from "../controller/conversation-controller.js";
+import { newMessage } from "../../client/src/service/Api.js";
 
 const route = express.Router();
 
@@ -8,5 +13,8 @@ route.post("/add", addUser);
 route.get("/users", getUser);
 
 route.post("/conversation/add", newConversation);
+route.post("/conversation/get", getConversation);
+
+route.post("/message/add", newMessage);
 
 export default route;
